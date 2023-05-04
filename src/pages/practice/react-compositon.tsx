46c985dd4fa6992863efc0599d3476055624ef7b
jihoon <ljhljh211@naver.com>
2023-05-04 10:08:18 +0900

    refactor: composition 실습코드 + TS 변환

--- src/pages/practice/react-compositon.tsx
+++ src/pages/practice/react-compositon.tsx
@@ -1,13 +1,15 @@
-import React, { useEffect, useState } from 'react'
+import React, { ReactNode, useEffect, useState } from 'react'
 import 'twin.macro'
 
-type TextProps = {
-  label: string
+interface Props {
+  children: ReactNode
   value: string
-  onChange: (inputValue: string) => void
+  onChange: (value: string) => void
   placeholder: string
-  error: string
+  error?: string
 }
+
+const TextFieldWithLabel = withError(withLabel(TextField))
 
 export default function ReactCompostionPage() {
   const [value, setValue] = useState('')
@@ -20,31 +22,59 @@ export default function ReactCompostionPage() {
   }, [])
 
   return (
-    <TextField
-      label="label text"
-      value={value}
-      onChange={setValue}
-      placeholder={'please enter...'}
-      error={error}
-    />
+    <div tw="bg-red-300 h-[100vh] text-center py-[200px] flex flex-col items-center">
+      <TextFieldWithLabel
+        value={value}
+        onChange={setValue}
+        placeholder={'please enter...'}
+        error={error}
+      >
+        {/* 자유도가 증가한다. */}
+        Label test ...222
+      </TextFieldWithLabel>
+    </div>
   )
 }
 
 /* 
   리팩토링 전
+
+  포인트
+  - label 안쓰고 싶을 땐 어떻게하는가?
+    - label 옵셔널로 바꾸기 => 그렇게 하면 코드가 조금 더 지저분해진다.
+    - HOC?
 */
 
-function TextField({ label, value, onChange, placeholder, error }: TextProps) {
+function withLabel<P extends Props>(
+  Component: React.ComponentType<P>,
+): React.FC<P> {
+  return ({ children, ...rest }: P) => (
+    <div>
+      {children && <label>{children}</label>}
+      <Component {...(rest as P)} />
+    </div>
+  )
+}
+
+function withError<P extends Props>(
+  Component: React.ComponentType<P>,
+): React.FC<P & { error?: string }> {
+  return ({ error, ...rest }: P & { error?: string }) => (
+    <div>
+      <Component {...(rest as P)} />
+      {error && <div tw="text-red-500 text-5xl">{error}</div>}
+    </div>
+  )
+}
+function TextField({ value, onChange, placeholder }: Props) {
   return (
-    <div tw="bg-red-300 h-[100vh] text-center py-[200px] flex flex-col items-center">
-      <label>{label}</label>
+    <div>
       <input
         value={value}
         type="text"
         placeholder={placeholder}
         onChange={e => onChange(e.target.value)}
       />
-      <div>{error}</div>
     </div>
   )
 }
