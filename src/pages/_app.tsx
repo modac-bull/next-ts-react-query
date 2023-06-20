05cef181df2f368a113870812f2ef5037139a66e
reconers-jihoon <ljhljh211@naver.com>
2023-06-21 08:59:45 +0900

    feat: add react-query example pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,18 @@ import type { AppProps } from 'next/app'
 import GlobalStyles from '../styles/GlobalStyles'
 import { ModalContextProvider } from '../context/ModalContext'
 import ModalContainer from '@/components/ui/modals/ModalContainer'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+const queryClient = new QueryClient()
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       <ModalContextProvider>
-        <GlobalStyles />
-        <Component {...pageProps} />
-        <ModalContainer />
+        <QueryClientProvider client={queryClient}>
+          <GlobalStyles />
+          <Component {...pageProps} />
+          <ModalContainer />
+        </QueryClientProvider>
       </ModalContextProvider>
     </>
   )
