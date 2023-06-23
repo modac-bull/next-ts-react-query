6136fbccaafb2f20ce40d18e1c90be3d3c9060b0
reconers-jihoon <ljhljh211@naver.com>
2023-06-23 13:54:10 +0900

    doc: typescript ch10.1.1 tutorials

--- sciprts/generic.ts
+++ sciprts/generic.ts
@@ -38,3 +38,38 @@
 
   identity(123)
 }
+
+{
+  function logWrapper<Input>(callback: (input: Input) => void) {
+    return (input: Input) => {
+      console.log('input', input)
+      callback(input)
+    }
+  }
+
+  // 타입 : (input : string) => void
+  logWrapper((input: string) => {
+    console.log(input.length)
+  })
+
+  // 타입 : (input: unknown) => void
+  logWrapper(input => {
+    console.log(input.length)
+    // Error
+  })
+
+  // 타입 : (input : string) => void
+  logWrapper<string>(input => {
+    console.log(input.length)
+  })
+
+  /* 
+  명시적 타입 인수는 항상 제네릭 함수에 지정할 수 있지만 때로는 필요하지 않을 수 있다.
+  필요할 때만 명시적 타입 인수를 지정한다.
+  */
+
+  // 타입 인수, 매개변수 타입 - 둘 중 하나는 제거 가능
+  logWrapper<string>((input: string) => {
+    console.log(input.length)
+  })
+}
