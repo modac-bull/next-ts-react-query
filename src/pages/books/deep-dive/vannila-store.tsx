762d5cfe0dfb0b75fcf1273442501ad0e3654ba6
leejihoon <ljhljh211@naver.com>
2023-11-22 21:09:27 +0900

    feat: createStore, 객체 속성 개별 적용 업데이트 되게 수정

--- src/pages/books/deep-dive/vannila-store.tsx
+++ src/pages/books/deep-dive/vannila-store.tsx
@@ -1,5 +1,6 @@
-import { createStore, useStore } from '@/utils/createStore'
-import React from 'react'
+import { createStore, useStore, useStoreSelector } from '@/utils/createStore'
+import { create } from 'domain'
+import React, { ChangeEvent, useCallback, useEffect } from 'react'
 
 const store = createStore({ count: 0 })
 
@@ -15,6 +16,9 @@ export default function VannilaStorePage() {
     <>
       <Counter1 />
       <Counter2 />
+
+      <Counter3 />
+      <TextEditor />
     </>
   )
 }
@@ -47,6 +51,51 @@ function Counter2() {
     <>
       <h3>Counter2 : {state.count}</h3>
       <button onClick={handleClick}>+</button>
+    </>
+  )
+}
+
+const storeObject = createStore({ count: 0, text: 'hi' })
+
+function Counter3() {
+  const counter = useStoreSelector(
+    storeObject,
+    useCallback(state => state.count, []),
+  )
+
+  function handleClick() {
+    storeObject.set(prev => ({ ...prev, count: prev.count + 1 }))
+  }
+
+  useEffect(() => {
+    console.log('counter rendered')
+  }, [])
+
+  return (
+    <>
+      <h3>{counter}</h3>
+      <button onClick={handleClick}>+</button>
+    </>
+  )
+}
+
+const textSelector = (state: ReturnType<typeof storeObject.get>) => state.text
+
+function TextEditor() {
+  const text = useStoreSelector(storeObject, textSelector)
+
+  useEffect(() => {
+    console.log('Counter Rendered')
+  })
+
+  function handleChange(e: ChangeEvent<HTMLInputElement>) {
+    storeObject.set(prev => ({ ...prev, text: e.target.value }))
+  }
+
+  return (
+    <>
+      <h3>Text</h3>
+      <input type="text" value={text} onChange={handleChange} />
     </>
   )
 }
--- src/utils/createStore.ts
+++ src/utils/createStore.ts
@@ -51,3 +51,20 @@ export const useStore = <State extends unknown>(store: Store<State>) => {
 
   return [state, store.set] as const
 }
+
+export const useStoreSelector = <State extends unknown, Value extends unknown>(
+  store: Store<State>,
+  selector: (state: State) => Value,
+) => {
+  const [state, setState] = useState(() => selector(store.get()))
+
+  useEffect(() => {
+    const unsubscribe = store.subscribe(() => {
+      const value = selector(store.get())
+      setState(value)
+    })
+    return unsubscribe
+  }, [store, selector])
+
+  return state
+}
