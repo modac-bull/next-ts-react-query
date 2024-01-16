0af776e5deff1c1e8dc6eb2276eb67a1e1079f17
leejihoon <ljhljh211@naver.com>
2024-01-16 21:53:48 +0900

    comment: 주석 추가

--- src/store/index.ts
+++ src/store/index.ts
@@ -35,10 +35,16 @@ const counterReducer: Reducer<CounterState, CounterAction> = (
   action,
 ) => {
   switch (action.type) {
+    // 타입 오타를 조심해야 한다.
     case 'INCREMENT':
+      // 절대 state를 직접 수정하면 안됨
+      // 왜냐면 redux는 state가 변경되었는지를 감지하기 위해
+      // 이전 state와 현재 state를 비교하기 때문
       return { counter: state.counter + 1, showCounter: state.showCounter }
     case 'INCREASE':
       return {
+        // 상태가 많아질수록 ...state를 사용하는 것이 좋다.
+        // 그래도 복잡해진다.
         counter: state.counter + action.amount,
         showCounter: state.showCounter,
       }
